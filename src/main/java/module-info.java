bf17b5bea0d2812555e5f4f51439513b14c23b0b
EfeEryildiz <yigitefem3@gmail.com>
2024-09-30 14:53:53 -0400

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module project.csc311group {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens project.csc311group to javafx.fxml;
+    exports project.csc311group;
+}
